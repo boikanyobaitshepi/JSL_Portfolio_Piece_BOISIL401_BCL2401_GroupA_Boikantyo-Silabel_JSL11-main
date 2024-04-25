85c87bc413d2f1a7a491ca15a9165e6ecb32c258
boikanyobaitshepi <156679487+boikanyobaitshepi@users.noreply.github.com>
2024-04-25 07:01:19 +0200

    Ensuring the column titles are set outside of this function or correctly initialized before this function runs

--- JSL_Portfolio_Piece_StudentNo_Classcode_Group_Name-Surname_JSL11-main/index.js
+++ JSL_Portfolio_Piece_StudentNo_Classcode_Group_Name-Surname_JSL11-main/index.js
@@ -105,12 +105,13 @@ const statusSelection = {
 // TASK: Fix Bugs
 function filterAndDisplayTasksByBoard(boardName) {
   const tasks = getTasks(); // Fetch tasks from a simulated local storage function
-  const filteredTasks = tasks.filter(task => task.board = boardName);
+  const filteredTasks = tasks.filter((task) => task.board = boardName);
 
   // Ensure the column titles are set outside of this function or correctly initialized before this function runs
 
-  elements.columnDivs.forEach(column => {
+  elements.columnDivs.forEach((column) => {
     const status = column.getAttribute("data-status");
+    const statusSelections = statusSelection[status];
     // Reset column content while preserving the column title
     column.innerHTML = `<div class="column-head-div">
                           <span class="dot" id="${status}-dot"></span>
